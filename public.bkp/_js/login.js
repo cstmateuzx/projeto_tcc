d4fddea542e8e544ccca9def8f671e6250637591
cstmateuzx <jennings1808@gmail.com>
2024-09-23 17:36:40 -0300

    public.bkp arquivos da branch master removida

---
+++ public.bkp/_js/login.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('login-form');
+    const messageElement = document.getElementById('message');
+
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+        const formData = new FormData(loginForm);
+
+        fetch('/login', {
+            method: 'POST',
+            body: JSON.stringify(Object.fromEntries(formData)),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'failed') {
+                    messageElement.className = 'error';
+                    messageElement.innerHTML = data.message;
+                } else if (data.status === 'success') {
+                    messageElement.className = 'success';
+                    messageElement.innerHTML = data.message;
+                    setTimeout(() => {
+                        window.location.href = '../_html/index.html';
+                    }, 1000);
+                }
+            })
+            .catch(error => {
+                messageElement.className = 'error';
+                messageElement.innerHTML = 'Ocorreu um erro ao processar sua solicitação.';
+            });
+    });
+});
